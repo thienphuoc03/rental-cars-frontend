a2eeed8b582abb2828531f71a3e4e3850d4d996a
thienphuoc <thienphuoc2391@gmail.com>
2023-12-18 19:31:07 +0700

    style: dark mode for car card

--- src/components/CarCard.tsx
+++ src/components/CarCard.tsx
@@ -30,7 +30,7 @@ const CarCard = ({
   return (
     <Link
       href={`car/${slug}`}
-      className="flex min-h-[356px] min-w-[180px] flex-col items-stretch justify-center gap-6 overflow-hidden rounded-lg border border-gray-100 bg-white p-4 text-white shadow-xl hover:scale-105 dark:bg-black xl:min-h-[398px]"
+      className="flex min-h-[356px] min-w-[180px] flex-col items-stretch justify-center gap-6 overflow-hidden rounded-lg border border-gray-100 bg-white p-4 text-black shadow-xl hover:scale-105 dark:bg-black dark:text-white xl:min-h-[398px]"
     >
       <div className="overflow-hidden rounded-lg">
         <Image src={thumbnail} alt="car" width={272} height={204} />
@@ -39,14 +39,15 @@ const CarCard = ({
       <div className="text-start">
         {/* feature car */}
         <div className="mb-2 flex items-center justify-start">
-          <span className="rounded-full bg-primary/10 px-2 py-1 text-xs text-[#262626]">
+          <span className="rounded-full bg-primary/10 px-2 py-1 text-xs text-[#262626] dark:bg-primary dark:text-white">
             {TransmissionEnum[transmission]}
           </span>
         </div>
 
         <h3 className="text-base font-bold capitalize">{name}</h3>
         <span className="text-xs text-gray-500">
-          <MapPin size={16} className="inline text-black" /> {address}
+          <MapPin size={16} className="inline text-black dark:text-white" />{' '}
+          {address}
         </span>
 
         <div className="my-4 h-[1px] w-full bg-gray-200" />
